c0728e5be1277a0b5f03a279be4fc6257a65aaf0
mnematolahi <116376570+mnematolahi@users.noreply.github.com>
2023-04-09 16:11:12 +0330

    Create windows install.bat

---
+++ windows install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Download file
+wget https://example.com/file.zip -O C:\path\to\save\file.zip
+
+REM Install package
+pip install requests
+
+REM Copy file
+copy C:\path\to\source\file.txt C:\path\to\destination\file.txt
+
+echo Done.
