32666652d85ac016b2022c010010b7364e1ce22b
mnematolahi <116376570+mnematolahi@users.noreply.github.com>
2023-04-09 17:22:45 +0330

    Update windows install.bat

--- windows install.bat
+++ windows install.bat
@@ -1,19 +1,44 @@
 @echo off
+
+REM to install Chocolatey from the command line on Windows
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo Installing Python...
+powershell.exe -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c choco install python3 -y'"
+
+echo Setting Python in PATH...
+setx PATH "%PATH%;C:\Python38\Scripts;C:\Python38"
+
+
+REM Install package
+echo Installing required packages...
+pip install requests wget
+wget https://github.com/nomic-ai/gpt4all/archive/refs/heads/main.zip
+tar -xvf gpt4all-main.zip
+cd gpt4all-main
+cd chat
+wget https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized.bin
+
+
+REM torch the cpu version
+pip install torch
+REM If you want to install the GPU version, you need to use the following command instead:
+REM pip install torch torchvision torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
+
+
+pip install accelerate datasets torchmetrics evaluate transformers wandb peft nodelist-inflator sentencepiece jsonlines
+
 echo Installing Microsoft Build Tools 2019...
 choco install visualstudio2019buildtools -y
-
-echo Installing Python package via pip...
-pip install torch-2.0.0+cu117-cp38-cp38-win_amd64.whl
-
-echo Done!
 
 REM Download file
 wget https://example.com/file.zip -O C:\path\to\save\file.zip
 
-REM Install package
-echo Installing Python package via pip...
-pip install torch-2.0.0+cu117-cp38-cp38-win_amd64.whl
-pip install requests
+REM now there is a .exe file in your folder. Run and Enjoy it!
+
+
+
+
 
 REM Copy file
 copy C:\path\to\source\file.txt C:\path\to\destination\file.txt
