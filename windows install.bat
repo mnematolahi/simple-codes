29e2e3bedee914110d55c35a6b1bb9fbe7f7fecc
mnematolahi <116376570+mnematolahi@users.noreply.github.com>
2023-04-09 16:26:36 +0330

    Update windows install.bat

--- windows install.bat
+++ windows install.bat
@@ -1,9 +1,18 @@
 @echo off
+echo Installing Microsoft Build Tools 2019...
+choco install visualstudio2019buildtools -y
+
+echo Installing Python package via pip...
+pip install torch-2.0.0+cu117-cp38-cp38-win_amd64.whl
+
+echo Done!
 
 REM Download file
 wget https://example.com/file.zip -O C:\path\to\save\file.zip
 
 REM Install package
+echo Installing Python package via pip...
+pip install torch-2.0.0+cu117-cp38-cp38-win_amd64.whl
 pip install requests
 
 REM Copy file
